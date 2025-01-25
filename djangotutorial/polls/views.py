c25f36ff4dcaba46739c7b08a5235867e6b2377c
massha <massha.leonova@yandex.ru>
2025-01-25 23:08:38 +0300

    create serializers

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,5 +1,36 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
+# , RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Product
+from .serializers import ProductSerializer
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class ProductView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_product_by_id(self):
+        id = request.data.get('id')
+        data = Product.objects.all.get(id=id)
+        ser = ProductSerializer(data)
+        return Response(ser.data)
+
+    def post(self, request, *args, **kwargs):
+        ser = ProductSerializer(data=request.data)
+        ser.save()
+        return Response(ser.data)
+
+
+
+
+
+
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
